0864fd22eb7d4d3b6cc8bb6dfd01abced58fee94
Johannes Elonheimo <johannes.elonheimo@gmail.com>
2022-11-27 15:25:16 +0200

    add tavaroita_korissa

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -3,11 +3,13 @@ from ostos import Ostos
 
 class Ostoskori:
     def __init__(self):
-        pass
+        self._ostokset = []
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
 
     def tavaroita_korissa(self):
-        pass
+        return sum(
+            [ostos.lukumaara() for ostos in self._ostokset]
+        )
         # kertoo korissa olevien tavaroiden lukumäärän
         # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
